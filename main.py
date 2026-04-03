f17a84e8d0b5b10571fab070cbb3a3271841b33b
Erica Barbato <erishine@gmail.com>
2026-04-03 12:18:36 +0100

    Skeleton project and setting up connection with Anthropic API

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ import os
 from dotenv import load_dotenv
 from anthropic import Anthropic
 
+load_dotenv()
 
 def main():
-    load_dotenv()
+    print("""
+        Gmail emails summary:\n
+          Specify the Gmail label associated to the emails and the number of days to consider:
+          \n\n
+          """)
+    label = input("Enter the Gmail label:")
+    days = int(input("Enter the days:"))
+
+    consent=input("Enter Y to confirm or N to stop the process:")
+    if consent.lower() != 'y':
+        exit()
+
+    print(f"Summarising the emails with label {label} received in the last {days} days")
+
 
     client = Anthropic(
         api_key=os.getenv('ANTHROPIC_API_KEY')
@@ -22,6 +36,7 @@ def main():
     )
 
     print(message.content[0].text)
+
 
 
 
