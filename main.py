51d9354de48cbe78e2da14ea33ec250c1826a4b1
Erica Barbato <erishine@gmail.com>
2026-04-05 11:55:35 +0100

    First version of the prompt, 20k tokens per day

--- main.py
+++ main.py
@@ -20,89 +20,85 @@ async def main():
         args=["run_gmail_mcp_server.py"]
     )
 
+    email_list = []
+
     async with GmailMcpClient() as client:
-            labels = await client.fetch_labels()
-            label_completer = WordCompleter(labels, ignore_case=True)
-            session_pt = PromptSession()
-            label = await session_pt.prompt_async("Enter Gmail label: ", completer=label_completer)
-
-            print(f"Summarising the emails with label '{label}'")
-
-            from_date = input("Enter start date in a format YYYY/MM/DD (default to last 7 days):")
-            if not from_date:
-                from_date = (datetime.today() - timedelta(days=7)).strftime("%Y/%m/%d")
-            else:
-                from_date = datetime.strptime(from_date, "%Y/%m/%d").strftime("%Y/%m/%d")
-
-            to_date = input("Enter end date in a format YYYY/MM/DD (default today):")
-            if not to_date:
-                to_date = datetime.today().strftime("%Y/%m/%d")
-            else:
-                to_date = datetime.strptime(to_date, "%Y/%m/%d").strftime("%Y/%m/%d")  
-
-            max_emails = input("Enter max number of emails to consider or enter for default(20 emails):")
-            if not max_emails:
-                max_emails=20
-            else:
-                max_emails=int(max_emails)
-
-            print(f"""
-                    Initating tasks:
-                        - email label: {label}
-                        - start date: {from_date}
-                        - end date: {to_date}
-                        - max emails: {max_emails}
-                  """)
-            consent=input("Press Enter to confirm or type anything (No) to stop the process:")
-            if consent != '':
-                exit()
-
-            email_ids = await client.search_emails(label, from_date, to_date, max_emails)
-
-            #result = await client.list_tools()
-            #print([t.name for t in result.tools])
-
-            email_list = []
-            for id in email_ids:
-                email_data = await client.get_email_message(id)
-                email = MyEmail.from_mcp(id, email_data)
-                print(email.clean_body,"\n\n")
-                email_list.append(email)
-   
-    """
-    
-    print(\"""
-        Gmail emails summary:\n
-          Specify the Gmail label associated to the emails and the number of days to consider:
-          \n\n
-          \""")
-    label = input("Enter the Gmail label:")
-    days = int(input("Enter the days:"))
-
-    consent=input("Enter Y to confirm or N to stop the process:")
-    if consent.lower() != 'y':
-        exit()
-
-    print(f"Summarising the emails with label {label} received in the last {days} days")
-
+        labels = await client.fetch_labels()
+        label_completer = WordCompleter(labels, ignore_case=True)
+        session_pt = PromptSession()
+        label = await session_pt.prompt_async("Enter Gmail label: ", completer=label_completer)
+        print(f"Summarising the emails with label '{label}'")
+        from_date = input("Enter start date in a format YYYY/MM/DD (default to last 7 days):")
+        if not from_date:
+            from_date = (datetime.today() - timedelta(days=7)).strftime("%Y/%m/%d")
+        else:
+            from_date = datetime.strptime(from_date, "%Y/%m/%d").strftime("%Y/%m/%d")
+        to_date = input("Enter end date in a format YYYY/MM/DD (default today):")
+        if not to_date:
+            to_date = datetime.today().strftime("%Y/%m/%d")
+        else:
+            to_date = datetime.strptime(to_date, "%Y/%m/%d").strftime("%Y/%m/%d")  
+        max_emails = input("Enter max number of emails to consider or enter for default(20 emails):")
+        if not max_emails:
+            max_emails=20
+        else:
+            max_emails=int(max_emails)
+        print(f"""
+                Initating tasks:
+                    - email label: {label}
+                    - start date: {from_date}
+                    - end date: {to_date}
+                    - max emails: {max_emails}
+              """)
+        consent=input("Press Enter to confirm or type anything (No) to stop the process:")
+        if consent != '':
+            exit()
+        email_ids = await client.search_emails(label, from_date, to_date, max_emails)
+        #result = await client.list_tools()
+        #print([t.name for t in result.tools])
+        print(f"\nFetching {len(email_ids)} emails.")
+        for id in email_ids:
+            email_data = await client.get_email_message(id)
+            email = MyEmail.from_mcp(id, email_data)
+            #print(email.clean_body,"\n\n")
+            email_list.append(email)
 
     client = Anthropic(
         api_key=os.getenv('ANTHROPIC_API_KEY')
     )
 
+    prompt = """
+        You are summarising emails and newsletters. For each email below, provide a brief summary of the key topics covered.
+        """
+          
+    for email in email_list:
+        email_info = f"""----
+                      Subject:{email.subject}
+                      Body:{email.clean_body}
+                      """
+        prompt += email_info
+
+    print(f"The prompt contains {len(prompt)} characters. It will be capped to 400k characters if greater.")
+
+    prompt = prompt[:400_000]
+
+
     message = client.messages.create(
-        max_tokens=1024,
+        max_tokens=4096,
         messages = [
             {
                 "role":"user",
-                "content":"hello Claude",
+                "content":prompt,
             }
         ],
         model="claude-haiku-4-5-20251001"
     )
 
-    print(message.content[0].text)
-"""
+    summary = message.content[0].text
+    print(summary)
+    with open('summary.md', 'w') as f:
+        f.write(summary)
+
 
 
 if __name__ == "__main__":
--- utils.py
+++
@@ -1,4 +0,0 @@
-class Utils:
-    # TODO implement clean_emails function
-    def clean_emails(raw_email_text):
-        return None
